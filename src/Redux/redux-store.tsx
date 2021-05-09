df5519aed810998444fd1dd1c44e3d8662feb728
gerasimenko.valery@gmail.com <Melomanka123*>
2021-05-10 00:23:35 +0300

    redux-form(login)-intro

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -5,16 +5,21 @@ import sidebarReducer from "./sidebar-reducer";
 import thunkMiddleware from "redux-thunk"
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
+import {reducer as formReducer} from "redux-form";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    authState: authReducer
+    authState: authReducer,
+    form: formReducer
 });
 
 let store = createStore(rootReducer, (applyMiddleware(thunkMiddleware)));
+
+//@ts-ignore
+window.store = store
 
 export default store;
 
